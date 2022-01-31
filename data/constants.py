07c2fb8f25f6b92f1618379a0a5ada73571c40db
Arseny Porkhunov <arseny2606yt@gmail.com>
2022-01-31 17:01:28 +0300

    part №1

--- data/constants.py
+++ data/constants.py
@@ -1,6 +1,12 @@
 name = "Map"
 
-width = 1280
-height = 720
+width = 600
+height = 450
 
 fps = 60
+
+geocoder_key = "40d1649f-0493-4b70-98ba-98533de7710b"
+search_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+
+start_lat = 55.803770
+start_lon = 37.635112
