b8c6ee238876b8a63687602e99766a6ea3350d9c
InferusAnima <InferusAnima@mail.ru>
2022-01-31 18:46:33 +0300

    part №2

--- data/states/map.py
+++ data/states/map.py
@@ -12,29 +12,31 @@ class Map:
     def __init__(self):
         self.screen = setup.screen
         self.buttons = pg.sprite.Group()
-        rect = self.screen.get_rect()
         self.x = 0
         self.y = 0
         self.z = 17
+        self.running = True
         self.map = get_map(f"ll={constants.start_lon},{constants.start_lat}&z={int(self.z)}")
         threading.Thread(target=self.get_map).start()
         self.draw_map()
 
     def get_map(self):
-        while True:
+        while self.running:
             self.map = get_map(f"ll={constants.start_lon},{constants.start_lat}&z={int(self.z)}")
 
     def draw_map(self):
         self.screen.blit(self.map, (0, 0))
 
-    def update(self, keys, clicks):
-        if keys[pg.K_PAGEUP]:
-            if self.z + 0.05 <= 17:
-                self.z += 0.05
-        if keys[pg.K_PAGEDOWN]:
-            if self.z - 0.05 >= 0.05:
-                self.z -= 0.05
+    def update_event(self, event):
+        self.running = False
 
-        print(self.z)
+    def update(self, keys, clicks, key_events):
+        for event in key_events:
+            if event.key == pg.K_PAGEUP:
+                if self.z + 1 <= 17:
+                    self.z += 1
+            elif event.key == pg.K_PAGEDOWN:
+                if self.z - 1 >= 0:
+                    self.z -= 1
         self.draw_map()
         return False
